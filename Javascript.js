41ea39754cef138235a92ad21fcb74cd415462c6
qknl <100494920+Qknl@users.noreply.github.com>
2023-12-04 22:59:16 -0500

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+function a() {
+    document.getElementById("sidenav").style.width = "30%";
+    document.getElementById("buttonopen").style.marginLeft = "-10em";
+    document.getElementById("buttonclose").style.marginLeft = "0em";
+}
+
+function b() {
+    document.getElementById("sidenav").style.width = "30px";
+    document.getElementById("buttonclose").style.marginLeft = "-10em";
+    document.getElementById("buttonopen").style.marginLeft = "0em";
+}
